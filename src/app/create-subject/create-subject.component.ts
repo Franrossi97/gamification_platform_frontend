d09b15012241f8ae50f978172fa525766691ac61
Franrossi97 <43619695+Franrossi97@users.noreply.github.com>
2022-05-27 00:35:53 -0300

    Add files via upload

--- src/app/create-subject/create-subject.component.ts
+++ src/app/create-subject/create-subject.component.ts
@@ -4,6 +4,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { SubjectClass } from '../shared/Subject';
 
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
+
 @Component({
   selector: 'app-create-subject',
   templateUrl: './create-subject.component.html',
@@ -14,6 +18,13 @@ export class CreateSubjectComponent implements OnInit {
   newSubjectForm: FormGroup;
   subject: SubjectClass=null;
   uploadPic;
+  fileName: string;
+  fileData: FormData;
+  fileS: ImageSnippet;
+  fileSizeError: boolean=false;
+  fileTypeError: boolean=false;
+  private loadingSubmit: boolean=false;
+
   @ViewChild('fform') newSubjectFormDirective;
 
   constructor(private fb: FormBuilder, private subjectService: SubjectService, private router: Router) { }
@@ -37,33 +48,64 @@ export class CreateSubjectComponent implements OnInit {
     //this.newSubjectForm.valueChanges.subscribe(data => {});
   }
 
-  onFileChanged(e)
-  {
-    this.uploadPic = e.target.files[0];
+  onImageChanged(imageInput){
+    this.fileSizeError=false;
+    this.fileTypeError=false;
+
+    const file: File = imageInput.files[0];
+    const reader= new FileReader();
+
+    if(file){
+      //reader.addEventListener('load', e =>{
+        this.fileS= new ImageSnippet('ok', file);
+      //});
+
+      if(this.fileS.file.size > 92160){
+        this.fileSizeError=true;
+      }
+
+      console.log(this.fileS.file.type);
+      if(this.fileS.file.type != 'image/png' && this.fileS.file.type != 'image/jpeg'){
+        this.fileTypeError=true;
+      }
+    }
   }
 
   onSubmit()
   {
-    //const uploadImageData = new FormData();
+    this.loadingSubmit=true;
+    const fileName: string=
+    `${this.newSubjectForm.get('year').value}_${this.newSubjectForm.get('career').value}_${this.fileS.file.name}`;
     this.subject=new SubjectClass(this.newSubjectForm.get('name').value,
     this.newSubjectForm.get('quarter').value, this.newSubjectForm.get('year').value, this.newSubjectForm.get('career').value,
-    0, true, true, null, +localStorage.getItem('userId'));
+    0, true, true, fileName, +localStorage.getItem('userId'));
 
-    //llamar a servicio
-    this.subjectService.createSubject(this.subject, localStorage.getItem('userId')).subscribe(res =>
+    if(this.newSubjectForm.get('image').value != null){
+      this.subjectService.uploadCoverImage(fileName, this.fileS.file).subscribe(res =>
+      {
+        this.submitRestOfForm();
+      });
+    }
+    else{
+      this.submitRestOfForm();
+    }
+
+  }
+
+  submitRestOfForm() {
+    this.subjectService.createSubject(this.subject, localStorage.getItem('userId')).subscribe(lastInsertedId =>
     {
       this.newSubjectForm.reset();
-      this.router.navigate([`/subject/${res}`]);
+      this.router.navigate([`/subject/${lastInsertedId}`]);
+
+      this.loadingSubmit=true;
     }, err =>
     {
       //Lanzar error
     });
-    /*
-    console.log(uploadImageData);
+  }
 
-    this.SubjectService.uploadCoverImage(this.subject.nombre+this.subject.anio, uploadImageData).subscribe(res =>
-    {
-      console.log('van las cosas bien');
-    });*/
+  getLoadingSubmit() {
+    return this.loadingSubmit;
   }
 }
